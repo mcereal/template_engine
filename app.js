9fcf72cc6cb3bbe43fd75845b0debb626a110bea
mcereal <mpbcurtis@gmail.com>
2020-11-10 11:39:05 -0500

    prompts and class creation funtioning

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Employee = require("./lib/Employee");
-const userPrompts = require("./userprompts.json");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -12,23 +11,98 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const promptUser = () => {
-  return inquirer.prompt([
+const employees = [];
+const USER_PROMPTS = {
+  newEmployee: [
+    {
+      type: "list",
+      name: "role",
+      message: "Choose your role: ",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
     {
       type: "input",
-      message: "What type of user do you want to add?",
-      name: "username",
+      name: "name",
+      message: "What is your name?",
     },
-  ]);
+    {
+      type: "input",
+      name: "id",
+      message: "What is your id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+    {
+      when: (selection) => {
+        return selection.role === "Manager";
+      },
+      type: "input",
+      name: "uniqueProperty",
+      message: "What is your office number?",
+    },
+    {
+      when: (selection) => {
+        return selection.role === "Engineer";
+      },
+      type: "input",
+      name: "uniqueProperty",
+      message: "What is your gitHub username?",
+    },
+    {
+      when: (selection) => {
+        return selection.role === "Intern";
+      },
+      type: "input",
+      name: "uniqueProperty",
+      message: "What school are you attending?",
+    },
+    {
+      type: "list",
+      name: "addAnotherEmployee",
+      message: "Would you like to add another employee?",
+      choices: ["Yes", "No"],
+    },
+  ],
+};
+
+const promptUser = () => inquirer.prompt(USER_PROMPTS.newEmployee);
+
+const newEmployee = (response) => {
+  if (response.role === "Manager") {
+    const newPerson = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.uniqueProperty
+    );
+    employees.push(newPerson);
+  } else if (response.role === "Engineer") {
+    const newPerson = new Engineer(
+      response.name,
+      response.id,
+      response.email,
+      response.uniqueProperty
+    );
+    employees.push(newPerson);
+  } else if (response.role === "Intern") {
+    const newPerson = new Intern(
+      response.name,
+      response.id,
+      response.email,
+      response.uniqueProperty
+    );
+    employees.push(newPerson);
+  }
 };
 
 promptUser()
-  .then((answers) => {
-    const content = generateREAMDME(answers);
-
-    return writeFileAsync("README.md", content);
-  })
-  .then(() => {
-    console.log("Successfully wrote README.md");
+  .then((response) => {
+    newEmployee(response);
+    if (response.addAnotherEmployee === "Yes") {
+      promptUser();
+    }
   })
   .catch((err) => console.log(err));
